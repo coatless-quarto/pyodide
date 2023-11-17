eb3b54b4542631c532965d6fca388cb9408901fb
James J Balamuta <coatless@users.noreply.github.com>
2023-11-17 06:20:51 +0000

    Built site for gh-pages

--- update-dev-quarto.sh
+++
@@ -1,4 +0,0 @@
-QUARTO_VERSION=1.4.506
-wget -O quarto-latest.deb https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.deb
-sudo dpkg -i ./quarto-latest.deb
-rm quarto-latest.deb
